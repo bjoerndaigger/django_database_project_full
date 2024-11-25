6845f7fa5e9d73b53b2a0191985c581706387e87
bjoerndaigger <bjoern.daigger@gmail.com>
2024-11-25 20:59:28 +0100

    first commit

---
+++ sales/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    newsletter_abo = models.BooleanField()
+    email_address = models.EmailField()
+    account = models.FloatField()
+
+
+
+
+
+
+
+
+
+
+
+
