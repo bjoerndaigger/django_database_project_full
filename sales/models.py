0f50ec01faf240da96e4ef5b7eb5169b7283bf61
bjoerndaigger <bjoern.daigger@gmail.com>
2024-11-26 11:08:20 +0100

    save and objects.create

--- sales/models.py
+++ sales/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Customer(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    newsletter_abo = models.BooleanField()
-    email_address = models.EmailField()
-    account = models.FloatField()
+    # newsletter_abo = models.BooleanField()
+    # email_address = models.EmailField()
+    # account = models.FloatField()
 
 
 
